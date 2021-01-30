d27cd0f62efcf8e680766b7f9dbdf60e0679a78c
AndreaTJ <andrea.torres.jaramillo@gmail.com>
2021-01-30 14:51:07 +0000

    Creation of: Base for templates and in-built database layer

--- application/__init__.py
+++ application/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db" 
+db = SQLAlchemy(app)
 
 from application import routes
---
+++ create.py
@@ -0,0 +1,3 @@
+from application import db 
+
+db.create_all()
