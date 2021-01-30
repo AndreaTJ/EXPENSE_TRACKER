55242f02dee4572bf9ae7eef28c1f8990978435d
AndreaTJ <andrea.torres.jaramillo@gmail.com>
2021-01-30 19:39:16 +0000

    Hiding SECRET_KEY (wtforms)

--- application/__init__.py
+++ application/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_object('config')
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db" 
 db = SQLAlchemy(app)
